08758ba9617c392df2f90537425a0ec02c6cf960
bragagustavo <gustavo.b.moraes2@gmail.com>
2025-03-20 11:48:06 -0300

    global style creation

---
+++ src/styles/global.js
@@ -0,0 +1,14 @@
+import { MD3LightTheme as DefaultTheme } from 'react-native-paper';
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#6200ea',
+    secondary: '#03dac5',
+    background: '#f8f8f8',
+    text: '#333',
+  },
+};
+
+export default theme;
